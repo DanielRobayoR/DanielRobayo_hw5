8405677b8643d849521d1bd119b7e7249177baed
CARLOS DANIEL ROBAYO <carlosdanielrobayo@ubuntu.(none)>
2017-12-02 15:57:14 -0500

    Subida 2

--- Plots.py
+++ Plots.py
@@ -2,14 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 Datos = np.genfromtxt('RadialVelocities.dat')
-#Parametros = np.genfromtxt('Datos_Ajuste.txt')
+Parametros = np.genfromtxt('Datos_Ajuste.txt')
 
 x = Datos[:,0]
 y = Datos[:,1]
 
-#M_b = Parametros[]
-#M_d = Parametros[]
-#M_h = Parametros[]
+M_b = Parametros[0]
+M_d = Parametros[1]
+M_h = Parametros[2]
+
+
 
 Velocidad_Ajuste = np.zeros(300)
 
@@ -18,7 +20,7 @@ def ajustado(Mb,Md,Mh,R):
 	Bd = 5.16;
 	Ad = 0.3105;
 	Ah = 64.3;
-	V = ((Mb**0.5)*(R)/(R**2+Bb**2)**0.75) + ((Md**0.5)*(R)/ (R**2+(Bd+Ad)**2)**0.75) + ((Mh**0.5)*(R)/(R**2+Ah**2)**0.25)
+	V = ((Mb**0.5)*(R)/(R**2+Bb**2)**0.75) + ((Md**0.5)*(R)/ (R**2+(Bd+Ad)**2)**0.75) + ((Mh**0.5)/(R**2+Ah**2)**0.25)
 	return V
 
 for i in range(0,300):
@@ -27,9 +29,8 @@ for i in range(0,300):
 
 plt.figure()
 plt.scatter(x, y, label='Observado', c='blue')
-plt.plot(x, Velocidad_Ajuste, label='Modelo', c='Black')
+plt.plot(x, Velocidad_Ajuste, label='Modelo', c='Red')
 plt.legend()
 plt.xlabel('Radio')
 plt.ylabel('Velocidad de Rotacion')
-plt.show()
-#plt.savefig("figura.png")
+plt.savefig("figura.png")
